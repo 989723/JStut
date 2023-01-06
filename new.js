022d6d8aa1c602646888fef7dec1cb97f9c99612
989723 <122037868+989723@users.noreply.github.com>
2023-01-07 00:42:02 +0530

    updateNew.js

--- new.js
+++ new.js
@@ -2,9 +2,7 @@ var items =document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[1]);
 items[1].textContent='Hello';
-items[0].style.fontWeight='bold';
-items[1].style.fontWeight='bold';
-items[2].style.fontWeight='bold';
-items[3].style.fontWeight='bold';
-
 items[2].style.backgroundColor='green'
+for (var i=0;i < items.length;i++){
+    items[i].style.backgroundColor= '#f4f4f4';
+}
