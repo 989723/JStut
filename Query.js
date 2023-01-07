61a61eaac9cb167bfeacf90de5e4866c78a5222a
989723 <122037868+989723@users.noreply.github.com>
2023-01-07 19:21:58 +0530

    Query.js

---
+++ Query.js
@@ -0,0 +1,7 @@
+
+var li =document.getElementsByClassName('list-group-item');
+li[2].style.backgroundColor='green'
+
+var secondItem = document.querySelector
+('.list-group-item:nth-child(3)');
+secondItem.style.color = 'white';
